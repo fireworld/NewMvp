8fb6afbfab3465a7bc3aa5a5bb90a3cee9cc1aa5
cxx <xx.ch@outlook.com>
2017-10-25 13:51:56 +0800

    mod IWebView

--- app/src/main/java/cc/colorcat/newmvp/web/IWebView.java
+++ app/src/main/java/cc/colorcat/newmvp/web/IWebView.java
@@ -1,48 +1,114 @@
 package cc.colorcat.newmvp.web;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
 
 /**
- * Created by cxx on 2017/2/13.
+ * Created by cxx on 2017/2/9.
  * xx.ch@outlook.com
  */
 public interface IWebView {
 
     /**
-     * 在此方法中创建新的 {@link android.webkit.WebView} 并添加到视图中
-     *
-     * @see WebViewDelegate#onCreateWebView()
+     * 在此方法中创建新的 {@link WebView} 并添加到视图中
      */
     void onCreateWebView();
 
     /**
-     * 在此方法中销毁原来的 {@link android.webkit.WebView} 并重新创建，然后添加到视图中
-     *
-     * @see WebViewDelegate#onRecreateWebView()
+     * 在此方法中销毁原来的 {@link WebView} 并重新创建，然后添加到视图中
      */
     void onRecreateWebView();
 
     /**
+     * @return 添加成功返回 true，否则返回 false
+     */
+    boolean addPageLoadListener(PageLoadListener listener);
+
+    /**
+     * @return 移除成功返回 true，否则返回 false
+     */
+    boolean removePageLoadListener(PageLoadListener listener);
+
+    /**
+     * @return 添加成功返回 true，否则返回 false
+     */
+    boolean addErrorListener(ErrorListener listener);
+
+    /**
+     * @return 移除成功返回 true，否则返回 false
+     */
+    boolean removeErrorListener(ErrorListener listener);
+
+    /**
      * 在此方法中载入 url
-     * note: 此方法必须在 {@link android.webkit.WebView} 创建后调用
+     * note: 此方法必须在 {@link WebView} 创建后调用
      *
      * @param url http / https
-     * @throws IllegalStateException 如果 {@link android.webkit.WebView} 没有创建，即为空的时候抛出此异常
+     * @throws IllegalStateException 如果 {@link WebView} 没有创建，即为空的时候抛出此异常
      */
     void loadUrl(String url);
 
     /**
-     * 刷新当前页面
+     * @param data     a String of data in the given encoding
+     * @param mimeType the MIME type of the data, e.g. 'text/html'
+     * @param encoding the encoding of the data
      */
-    void refresh();
+    void loadData(String data, String mimeType, String encoding);
 
     /**
-     * 在此方法中销毁 {@link android.webkit.WebView}, 回收 {@link android.webkit.WebView} 的资源
+     * 刷新当前页面
+     */
+    void reload();
+
+    /**
+     * @param script         the JavaScript to execute.
+     * @param resultCallback A callback to be invoked when the script execution
+     *                       completes with the result of the execution (if any).
+     *                       May be null if no notificaion of the result is required.
+     */
+    void evaluateJavascript(String script, ValueCallback<String> resultCallback);
+
+    /**
+     * @return {@link WebView.HitTestResult#getType()}
+     */
+    int getHitTestType();
+
+    /**
+     * 获取上下文环境变量
+     */
+    Context getContext();
+
+    /**
+     * 在此方法中销毁 {@link WebView}, 回收 {@link WebView} 的资源
      */
     void onDestroyWebView();
 
-    /**
-     * @return 上下文环境，如可能需要在新的 {@link android.app.Activity} 中打开地址，此时需要 Context
-     */
-    Context getContext();
+
+    interface PageLoadListener {
+
+        void onPageStarted(String url, Bitmap favicon);
+
+        void onPageFinished(String url);
+    }
+
+
+    interface ErrorListener {
+
+        void onReceivedError(int errorCode, String description, String failingUrl);
+    }
+
+
+    class SimplePageLoadListener implements PageLoadListener {
+        @Override
+        public void onPageStarted(String url, Bitmap favicon) {
+
+        }
+
+        @Override
+        public void onPageFinished(String url) {
+
+        }
+    }
 }
--- app/src/main/java/cc/colorcat/newmvp/web/WebViewDelegate.java
+++ app/src/main/java/cc/colorcat/newmvp/web/WebViewDelegate.java
@@ -4,57 +4,90 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.view.ViewGroup;
+import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import cc.colorcat.newmvp.util.Op;
+import java.util.LinkedList;
+import java.util.List;
+
 
 /**
- * Created by cxx on 2017/2/13.
+ * Created by cxx on 17-3-24.
  * xx.ch@outlook.com
  */
 public class WebViewDelegate implements IWebView {
     private static final int RETRY_LIMIT = 3;
 
-    private ViewGroup mGroup;
+    private ViewGroup mWebContainer;
     private WebView mWebView;
     private IUriHandler mHandler;
-    private PageLoadListener mListener;
 
-    public static WebViewDelegate create(@NonNull ViewGroup view, @NonNull IUriHandler handler) {
-        return new WebViewDelegate(view, handler);
-    }
+    private List<PageLoadListener> mPageListeners = new LinkedList<>();
+    private List<ErrorListener> mErrorListeners = new LinkedList<>();
 
-    private WebViewDelegate(@NonNull ViewGroup group, @NonNull IUriHandler handler) {
-        mGroup = Op.nonNull(group, "group == null");
-        mHandler = Op.nonNull(handler, "handler == null");
-    }
-
-    public void setPageLoadListener(PageLoadListener listener) {
-        mListener = listener;
+    public WebViewDelegate(@NonNull ViewGroup webContainer, @NonNull IUriHandler handler) {
+        mWebContainer = webContainer;
+        mHandler = handler;
     }
 
     @Override
     public void onCreateWebView() {
-        mWebView = new WebView(mGroup.getContext());
+        mWebView = new WebView(mWebContainer.getContext());
         configWebView(mWebView, mHandler);
-        mGroup.addView(mWebView);
+        mWebContainer.addView(mWebView);
     }
 
     @SuppressLint("SetJavaScriptEnabled")
-    protected void configWebView(WebView view, IUriHandler handler) {
-        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        view.setWebViewClient(new LowerClient(this, handler));
-        WebSettings settings = view.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);
-        settings.setBuiltInZoomControls(false);
-        settings.setSupportMultipleWindows(true);
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+    protected void configWebView(WebView webView, IUriHandler handler) {
+        webView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        webView.setWebViewClient(new LowerClient(this, handler));
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true); //支持js
+        settings.setJavaScriptCanOpenWindowsAutomatically(true); // 通过 js 打开新窗口
+
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); //支持内容重新布局
+        settings.setSupportMultipleWindows(true); //多窗口
+
+        /* 缓存相关开始*/
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+//        settings.setAllowFileAccess(true); // 设置可以访问文件
+//        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+
+        settings.setAppCacheEnabled(true);
+        settings.setAppCachePath(webView.getContext().getCacheDir().getAbsolutePath());
+//        settings.setAppCacheMaxSize(10 * 1024 * 1024);
+
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+//        settings.setDatabasePath(webView.getContext().getCacheDir().getAbsolutePath());
+        /* 缓存相关结束*/
+
+//        settings.setPluginState(WebSettings.PluginState.ON);
+
+//        webView.requestFocusFromTouch();
+//        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        // LOLLIPOP 及之后默认禁止 https 和 http 混合，需手动开启
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
     }
 
     @Override
@@ -64,21 +97,64 @@ public class WebViewDelegate implements IWebView {
     }
 
     @Override
+    public boolean addPageLoadListener(PageLoadListener listener) {
+        if (listener == null) throw new NullPointerException("listener == null");
+        return !mPageListeners.contains(listener) && mPageListeners.add(listener);
+    }
+
+    @Override
+    public boolean removePageLoadListener(PageLoadListener listener) {
+        return listener != null && mPageListeners.remove(listener);
+    }
+
+    @Override
+    public boolean addErrorListener(ErrorListener listener) {
+        if (listener == null) throw new NullPointerException("listener == null");
+        return !mErrorListeners.contains(listener) && mErrorListeners.add(listener);
+    }
+
+    @Override
+    public boolean removeErrorListener(ErrorListener listener) {
+        return listener != null && mErrorListeners.remove(listener);
+    }
+
+    @Override
     public void loadUrl(String url) {
-        if (mWebView == null) {
-            throw new IllegalStateException("You must call onCreateWebView() before");
-        }
         mWebView.loadUrl(url);
     }
 
     @Override
-    public void refresh() {
+    public void loadData(String data, String mimeType, String encoding) {
+        mWebView.loadData(data, mimeType, encoding);
+    }
+
+    @Override
+    public void reload() {
         mWebView.reload();
     }
 
     @Override
+    public void evaluateJavascript(String script, ValueCallback<String> resultCallback) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mWebView.evaluateJavascript(script, resultCallback);
+        } else {
+            mWebView.loadUrl("javascript:" + script);
+        }
+    }
+
+    @Override
+    public int getHitTestType() {
+        return mWebView.getHitTestResult().getType();
+    }
+
+    @Override
+    public Context getContext() {
+        return mWebContainer.getContext();
+    }
+
+    @Override
     public void onDestroyWebView() {
-        mGroup.removeAllViews();
+        mWebContainer.removeAllViews();
         if (mWebView != null) {
             mWebView.removeAllViews();
             mWebView.destroy();
@@ -86,46 +162,32 @@ public class WebViewDelegate implements IWebView {
         }
     }
 
-    @Override
-    public Context getContext() {
-        return mGroup.getContext();
+    public WebView getWebView() {
+        return mWebView;
     }
 
-    protected void notifyPageStarted(WebView view, String url) {
-        if (mListener != null) {
-            mListener.onPageStarted(view, url);
+    private void notifyPageStarted(String url, Bitmap favicon) {
+        for (PageLoadListener listener : mPageListeners) {
+            listener.onPageStarted(url, favicon);
         }
     }
 
-    protected void notifyPageFinished(WebView view, String url) {
-        if (mListener != null) {
-            mListener.onPageFinished(view, url);
+    private void notifyPageFinished(String url) {
+        for (PageLoadListener listener : mPageListeners) {
+            listener.onPageFinished(url);
         }
     }
 
-    public interface PageLoadListener {
-
-        void onPageStarted(WebView view, String url);
-
-        void onPageFinished(WebView view, String url);
-    }
-
-    public static class SimplePageLoadListener implements PageLoadListener {
-        @Override
-        public void onPageStarted(WebView view, String url) {
-
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-
+    private void notifyReceivedError(int errorCode, String description, String failingUrl) {
+        for (ErrorListener listener : mErrorListeners) {
+            listener.onReceivedError(errorCode, description, failingUrl);
         }
     }
 
 
-    private static class BaseWebViewClient extends WebViewClient {
-        IUriHandler mHandler;
+    public static class BaseWebViewClient extends WebViewClient {
         WebViewDelegate mDelegate;
+        IUriHandler mHandler;
         boolean mHasError = false;
         int mRetryCount = 0;
 
@@ -135,18 +197,23 @@ public class WebViewDelegate implements IWebView {
         }
 
         @Override
-        public final void onPageStarted(WebView view, String url, Bitmap favicon) {
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
-            mDelegate.notifyPageStarted(view, url);
+            mDelegate.notifyPageStarted(url, favicon);
         }
 
         @Override
-        public final void onPageFinished(WebView view, String url) {
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            mDelegate.notifyPageFinished(view, url);
+            mDelegate.notifyPageFinished(url);
             if (mHasError) {
                 if (mRetryCount < RETRY_LIMIT) {
-                    mDelegate.refresh(); // 尝试重新加载
+                    mDelegate.reload(); // 尝试重新加载
                     mRetryCount++; // 重新加载记数
                 } else {
                     mRetryCount = 0; // 重新加载次数达到 RETRY_LIMIT 后，将 RetryCount 置 0，以记载其它 url 失败尝试次数
@@ -160,6 +227,7 @@ public class WebViewDelegate implements IWebView {
             }
         }
     }
+
 
     private static class LowerClient extends BaseWebViewClient {
 
@@ -178,40 +246,39 @@ public class WebViewDelegate implements IWebView {
         public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
             super.onReceivedError(view, errorCode, description, failingUrl);
             mHasError = true; // 标示当前 url 加载失败
+            mDelegate.notifyReceivedError(errorCode, description, failingUrl);
         }
     }
 
 
-//    /**
-//     * 用于系统版本大于或等于 Build.VERSION_CODES.M 的手机
-//     */
-//    @RequiresApi(api = Build.VERSION_CODES.M)
-//    private static class LaterClient extends BaseWebViewClient {
-//
-//        LaterClient(WebViewDelegate delegate, IUriHandler handler) {
-//            super(delegate, handler);
-//        }
-//
-//        /**
-//         * 此方法调用时机未知，在点击链接或后台轮播时并不一定会进入此方法
-//         */
-//        @Override
-//        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-//            Uri uri = request.getUrl();
-//            MiLog.i(TAG + "Later", "Override: " + uri.toString());
-//            return mHandler.handle(uri, mDelegate) || super.shouldOverrideUrlLoading(view, request);
-//        }
-//
-//        @Override
-//        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-//            int code = error.getErrorCode();
-//            if (code == WebViewClient.ERROR_UNSUPPORTED_SCHEME
-//                    && mHandler.handle(request.getUrl(), mDelegate)) {
-//                return;
-//            }
-//            mHasError = true;
-//            MiLog.i(TAG + "Later", "ErrorCode: " + code + "\nDescription: " + error.getDescription() + "\nFailingUrl: " + request.getUrl().toString());
-//            super.onReceivedError(view, request, error);
-//        }
-//    }
+    /**
+     * 用于系统版本大于或等于 Build.VERSION_CODES.M 的手机
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private static class LaterClient extends BaseWebViewClient {
+
+        LaterClient(WebViewDelegate delegate, IUriHandler handler) {
+            super(delegate, handler);
+        }
+
+        /**
+         * 在点击链接或后台轮播时不一定会进入此方法，需注意
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            Uri uri = request.getUrl();
+            return mHandler.handle(uri, mDelegate) || super.shouldOverrideUrlLoading(view, request);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            int code = error.getErrorCode();
+            if (code == WebViewClient.ERROR_UNSUPPORTED_SCHEME && mHandler.handle(request.getUrl(), mDelegate)) {
+                return;
+            }
+            mHasError = true;
+            super.onReceivedError(view, request, error);
+        }
+    }
 }
+
